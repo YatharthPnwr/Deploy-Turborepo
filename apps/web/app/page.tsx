ef259f039830d4efcbce8f8234cd5db2fc7149da
Yatharth <yatharthpawr@gmail.com>
2025-12-02 14:06:11 +0530

    Changed the page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,7 +4,7 @@ export default async function Home() {
   const user = await client.user.findFirst();
   return (
     <div>
-      <div>These are the user details</div>
+      <div>These are the user </div>
       username: {user?.username} <br />
       password: {user?.password}
     </div>
